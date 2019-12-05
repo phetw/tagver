ce083e71d6d94f027f8c2884d48e63f8516e2ca4
Tremayne Christ <tremaynechrist@gmail.com>
2019-12-05 16:58:29 +0000

    fix: use local ref for comparing against remote

--- git-helper.js
+++ git-helper.js
@@ -42,14 +42,14 @@ exec(`git tag -a ${tag} -m "${message}"`, options)
 }).then(() => tag);
 
 
-// Gets the default or provided branch
-const branch = options => options.branch ? Promise.resolve(options.branch) : exec('git rev-parse --abbrev-ref HEAD', options);
+// Gets the current local ref/branch
+const branch = options => exec('git rev-parse --abbrev-ref HEAD', options);
 
 
 // Checks to see if the current local state of the repository is in sync with its remote
 const checkStatus = options =>
 fetch(options).then(() => branch(options)).then(branch =>
-exec(`git status --porcelain && git log ${branch}..origin/${branch} --oneline`, options))
+exec(`git status --porcelain && git log ${branch}..origin/${options.branch || branch} --oneline`, options))
 .then(stdout => !stdout);
 
 
